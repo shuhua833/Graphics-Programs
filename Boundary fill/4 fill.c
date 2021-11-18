8c7ab9c8f7a682f1d9d9ae669e500fc4304b4002
Shuhua Shrestha <71085603+shuhua833@users.noreply.github.com>
2021-11-18 18:12:57 +0545

    Create 4 fill.c

---
+++ Boundary fill/4 fill.c
@@ -0,0 +1,24 @@
+#include<stdio.h>  
+#include<conio.h>  
+#include<graphics.h>  
+void flood(int,int,int,int);  
+int main()  
+{  
+    int gd=DETECT,gm;  
+    initgraph(&gd,&gm,"C:/TURBOC3/bgi");  
+    rectangle(50,50,250,250);  
+    flood(55,55,10,0);  
+    getch();  
+}  
+void flood(int x,int y,int fillColor, int defaultColor)  
+{  
+    if(getpixel(x,y)==defaultColor)  
+    {  
+          
+        putpixel(x,y,fillColor);  
+        flood(x+1,y,fillColor,defaultColor);  
+        flood(x-1,y,fillColor,defaultColor);  
+        flood(x,y+1,fillColor,defaultColor);  
+        flood(x,y-1,fillColor,defaultColor);  
+    }  
+}  
